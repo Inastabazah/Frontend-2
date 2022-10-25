eeb93c08fda46edccc6053d83bd1914e4b31844c
nada <inastabazah1@gmail.com>
2022-10-25 11:54:18 +0300

    save

---
+++ javascript.js
@@ -0,0 +1,5 @@
+onload=function(){
+    document.querySelector('h1').style.backgroundColor='red';
+    document.getElementsByClassName('p')[0].style.backgroundColor='blue';
+    document.getElementById('h2').style.backgroundColor='green'
+}
