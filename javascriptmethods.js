29a0fbed1c08334e616eb4ade1aabc44c90f99c1
nada <inastabazah1@gmail.com>
2022-10-28 15:02:36 +0300

    save

---
+++ javascriptmethods.js
@@ -0,0 +1,12 @@
+let par = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis accusamus quis minima provident modi placeat, consectetur aliquam odit animi quos voluptatum voluptatem cum beatae aliquid assumenda nulla eaque ea"
+console.log(par.endsWith ('m', 15));
+console.log(par.startsWith ('s'));
+console.log(par.includes ('se'));
+console.log(par.length );
+console.log(par.[25]);
+console.log(par.charAt (17));
+console.log(par.includes ('e'));
+console.log(par.lastIndexOf ('a'));
+console.log(par.slice (25, 40));
+console.log(par.split ('rem');
+console.log(par.substring(25, 40));
